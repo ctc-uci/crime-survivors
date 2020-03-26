8ed46f6e6dda012f777095bfb46562ae85f062de
Pursain <harrisonhuang00@gmail.com>
2020-03-26 01:38:51 -0700

    eslint compliance

--- test/src/components/ExampleReactFunctional.js
+++ test/src/components/ExampleReactFunctional.js
@@ -1,15 +1,19 @@
-import React from "react"
-import PropTypes from "prop-types"
-import uuidv4 from "uuid"
+import React from 'react';
+import PropTypes from 'prop-types';
+import uuidv4 from 'uuid';
 
 // This is a functional react component (no states, only props)
-const ExampleReactFunctional = props => {
+const ExampleReactFunctional = (props) => {
+  // deconstruct props, required by eslint
+  const { resource } = props;
+
   return (
     <div>
-      <h2>{props.resource.title}</h2>
-      <h3>{props.resource.desc}</h3>
-      {props.resource.phone.map(phoneInfo => (
-        <div key={uuidv4() /*uuid bc array of items*/}>  
+      <h2>{resource.title}</h2>
+      <h3>{resource.desc}</h3>
+      <h3>{resource.category}</h3>
+      {resource.phone.map((phoneInfo) => (
+        <div key={uuidv4() /* uuid bc array of items */}>
           <h3>
             {phoneInfo.desc}
             <br />
@@ -17,15 +21,24 @@ const ExampleReactFunctional = props => {
           </h3>
         </div>
       ))}
-      <h3>{props.resource.website}</h3>
-      <h3>{props.resource.hours}</h3>
-      <h3>{props.resource.address}</h3>
+      <h3>{resource.address}</h3>
       <br />
     </div>
-  )
-}
+  );
+};
 
-// Type validation on incoming types (optional but plz do it)
+// Defaults values for props, required by eslint
+ExampleReactFunctional.defaultProps = {
+  resource: {
+    address: 'address',
+    category: 'category',
+    phone: [{ desc: 'desc', number: '(555) 555-5555' }],
+    title: 'title',
+    desc: 'desc',
+  },
+};
+
+// Proptype validation, required by eslint
 ExampleReactFunctional.propTypes = {
   resource: PropTypes.shape({
     address: PropTypes.string,
@@ -34,11 +47,11 @@ ExampleReactFunctional.propTypes = {
       PropTypes.shape({
         desc: PropTypes.string,
         number: PropTypes.string,
-      })
+      }),
     ),
     title: PropTypes.string,
     desc: PropTypes.string,
   }),
-}
+};
 
-export default ExampleReactFunctional
+export default ExampleReactFunctional;
--- test/src/pages/examplepage.js
+++ test/src/pages/examplepage.js
@@ -1,30 +1,75 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import ExampleReactFunctional from "../components/ExampleReactFunctional"
-import ExampleReactStateful from "../components/ExampleReactStateful"
-import uuidv4 from "uuid"
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import uuidv4 from 'uuid';
+import ExampleReactFunctional from '../components/ExampleReactFunctional';
+import ExampleReactStateful from '../components/ExampleReactStateful';
 
-const ExamplePage = ({data}) => {
+const ExamplePage = ({ data }) => {
+  // deconstruct props, required by eslint
+  const { allOrangeYaml } = data;
+  const { nodes } = allOrangeYaml;
+
   return (
     <div>
-    <h1>Functional</h1>
-    {data.allOrangeYaml.nodes.map(data=>(
-      <div key={uuidv4()}>
-      <ExampleReactFunctional  resource={data}/>
-      <hr></hr>
-      </div>
-    ))}
-    <h1>Stateful</h1>
-    {data.allOrangeYaml.nodes.map(data=>(
-      <div key={uuidv4()}>
-      <ExampleReactStateful key={uuidv4()} resource={data}/>
-      <hr></hr>
-      </div>
-    ))}
+      <h1>Functional Components</h1>
+      {nodes.map((node) => (
+        <div key={uuidv4() /* uuid bc array of items */}>
+          <ExampleReactFunctional resource={node} />
+          <hr />
+        </div>
+      ))}
+      <h1>Stateful Components</h1>
+      {nodes.map((node) => (
+        <div key={uuidv4() /* uuid bc array of items */}>
+          <ExampleReactStateful resource={node} />
+          <hr />
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
+// Defaults values for props, required by eslint
+ExamplePage.defaultProps = {
+  data: {
+    allOrangeYaml: {
+      nodes: [{
+        resource: {
+          address: 'address',
+          category: 'category',
+          phone: [{ desc: 'desc', number: '(555) 555-5555' }],
+          title: 'title',
+          desc: 'desc',
+        },
+      }],
+    },
+  },
+};
+
+// Proptype validation, required by eslint
+ExamplePage.propTypes = {
+  data: PropTypes.shape({
+    allOrangeYaml: PropTypes.shape({
+      nodes: PropTypes.arrayOf({
+        resource: PropTypes.shape({
+          address: PropTypes.string,
+          category: PropTypes.string,
+          phone: PropTypes.arrayOf(
+            PropTypes.shape({
+              desc: PropTypes.string,
+              number: PropTypes.string,
+            }),
+          ),
+          title: PropTypes.string,
+          desc: PropTypes.string,
+        }),
+      }),
+    }),
+  }),
+};
+
+// Standard GraphQL query for YAML, play around with graphql on localhost:8000/__graphql
 export const query = graphql`
   query MyQuery {
     allOrangeYaml {
@@ -40,6 +85,6 @@ export const query = graphql`
       }
     }
   }
-`
+`;
 
-export default ExamplePage
+export default ExamplePage;
