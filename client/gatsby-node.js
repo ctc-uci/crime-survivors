4bc36d531ef25ec4d07e7c2962b1dfcadca63ce6
Justine Koa <justinekoa@gmail.com>
2020-04-01 08:45:58 +0000

    JK routing

--- client/gatsby-node.js
+++ client/gatsby-node.js
@@ -1,7 +1,67 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path');
 
-// You can delete this file if you're not using it
+
+const fixStringForWindows = (stringToFix) => {
+  if (stringToFix != null) {
+    return stringToFix.replace(/[^a-zA-Z ]/g, '');
+  }
+  return stringToFix;
+};
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  // we use a Promise to make sure the data are loaded
+  // before attempting to create the pages with them
+  return new Promise((resolve, reject) => {
+    // fetch your data here, generally with graphQL.
+    graphql(`
+      {
+        allOrangeCountyYaml {
+          edges {
+            node {
+              id
+              title
+              category
+            }
+          }
+        }
+      }
+    `).then((result) => {
+      // first check if there is no errors
+      if (result.errors) {
+        // reject Promise if error
+        reject(result.errors);
+      }
+      const categoriesLoaded = new Set();
+      // if no errors, you can map into the data and create your static pages
+      result.data.allOrangeCountyYaml.edges.forEach(({ node }) => {
+        // create page according to the fetched data
+        createPage({
+          path: `/${fixStringForWindows(node.category)}/${fixStringForWindows(node.title)}`, // your url -> /categories/orgtitle
+          component: path.resolve('./src/templates/resources.js'), // your template component
+          context: {
+            id: node.id,
+            // optional,
+            // data here will be passed as props to the component `this.props.pathContext`,
+            // as well as to the graphql query as graphql arguments.
+          },
+        });
+
+        if (!categoriesLoaded.has(node.category)) {
+          categoriesLoaded.add(node.category);
+          createPage({
+            path: `/${fixStringForWindows(node.category)}/${fixStringForWindows(node.title)}`, // your url -> /categories/orgtitle
+            component: path.resolve('./src/templates/categories.js'), // your template component
+            context: {
+              category: node.category,
+              id: node.id,
+            },
+          });
+        }
+      });
+
+      resolve();
+    });
+  });
+};
---
+++ client/src/templates/categories.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import { v4 as uuidv4 } from 'uuid';
+
+const CategoryPage = ({ data }) => {
+  const { allOrangeCountyYaml } = data;
+  const { nodes } = allOrangeCountyYaml;
+
+  // simply displays all resources for this category in an ugly list
+  return (
+    <div>
+      {nodes.map((resource) => (
+        <div key={uuidv4() /* uuid bc array of items */}>
+          <h1>{resource.title}</h1>
+          <div>{resource.desc}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+// we can query the needed resources according to the category passed in the
+// context property of createPage() in gatsby-node.js
+export const query = graphql`
+  query CategoryQuery($category: String!) {
+    allOrangeCountyYaml(filter: {category: { eq: $category }}) {
+      nodes {
+        id
+        category
+        title
+        desc
+        phone {
+          desc
+          number
+        }
+        website
+        email
+        hours
+        address
+      }
+    }
+  }
+`;
+
+// Defaults values for props, required by eslint
+CategoryPage.defaultProps = {
+  data: {
+    allOrangeCountyYaml: {
+      nodes: [{
+        address: 'address',
+        category: 'category',
+        phone: [{ desc: 'desc', number: '(555) 555-5555' }],
+        title: 'title',
+        desc: 'desc',
+      },
+      ],
+    },
+  },
+};
+
+
+// Proptype validation, required by eslint
+CategoryPage.propTypes = {
+  data: PropTypes.shape({
+    allOrangeCountyYaml: PropTypes.shape({
+      nodes: PropTypes.arrayOf(
+        PropTypes.shape({
+          address: PropTypes.string,
+          category: PropTypes.string,
+          phone: PropTypes.arrayOf(
+            PropTypes.shape({
+              desc: PropTypes.string,
+              number: PropTypes.string,
+            }),
+          ),
+          title: PropTypes.string,
+          desc: PropTypes.string,
+        }),
+      ),
+    }),
+  }),
+};
+
+export default CategoryPage;
