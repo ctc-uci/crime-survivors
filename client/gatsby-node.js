eeb7485db77d8d83048077997d58d93e83d925c3
Pursain <harrisonhuang00@gmail.com>
2020-04-10 21:32:04 -0700

    refactored createPages

--- client/gatsby-node.js
+++ client/gatsby-node.js
@@ -8,62 +8,103 @@ const fixStringForWindows = (stringToFix) => {
   return stringToFix;
 };
 
-exports.createPages = ({ graphql, actions }) => {
+const generatePath = (category, title) => {
+  if (title === '') {
+    return `/${fixStringForWindows(category)}`;
+  }
+  return `/${fixStringForWindows(category)}/${fixStringForWindows(title)}`;
+};
+
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
+
+  const queryYamlNames = ['allOrangeCountyYaml'];
+
+  const queryResources = `
+    {
+      allOrangeCountyYaml(filter: {title: {ne: null}}) {
+        edges {
+          node {
+            id
+            title
+            category
+          }
+        }
+      }
+    }`;
+
+  const queryCategories = `
+    {
+      allOrangeCountyYaml {
+        group(field: category) {
+          fieldValue
+        }
+      }
+    }`;
+
+  const generateResourcePages = (query, names) => new Promise((resolve, reject) => {
+    graphql(query).then((result) => {
+      if (result.errors) {
+        reject(result.errors);
+      }
+
+      names.forEach((name) => {
+        // if no errors, you can map into the data and create your static pages
+        result.data[name].edges.forEach(({ node }) => {
+          // create page according to the fetched data
+          createPage({
+            path: generatePath(node.category, node.title), // your url -> /categories/orgtitle
+            component: path.resolve('./src/templates/resources.js'), // your template component
+            // component: path.resolve('./src/components/resourcePageContainer.js'),
+            context: {
+              id: node.id,
+              // optional,
+              // data here will be passed as props to the component `this.props.pathContext`,
+              // as well as to the graphql query as graphql arguments.
+            },
+          });
+        });
+      });
+      resolve();
+    });
+  });
+
+  const generateCategoryPages = (query, names) => new Promise((resolve, reject) => {
+    graphql(query).then((result) => {
+      if (result.errors) {
+        reject(result.errors);
+      }
+
+      names.forEach((name) => {
+        // if no errors, you can map into the data and create your static pages
+        result.data[name].group.forEach((category) => {
+          // create page according to the fetched data
+          createPage({
+            path: generatePath(category.fieldValue, ''), // your url -> /categories/orgtitle
+            component: path.resolve('./src/templates/categories.js'), // your template component
+            // component: path.resolve('./src/components/resourcePageContainer.js'),
+            context: {
+              category: category.fieldValue,
+              // optional,
+              // data here will be passed as props to the component `this.props.pathContext`,
+              // as well as to the graphql query as graphql arguments.
+            },
+          });
+        });
+      });
+      resolve();
+    });
+  });
 
   // we use a Promise to make sure the data are loaded
   // before attempting to create the pages with them
   return new Promise((resolve, reject) => {
-    // fetch your data here, generally with graphQL.
-    graphql(`
-      {
-        allOrangeCountyYaml {
-          edges {
-            node {
-              id
-              title
-              category
-            }
-          }
-        }
-      }
-    `).then((result) => {
-      // first check if there is no errors
-      if (result.errors) {
-        // reject Promise if error
-        reject(result.errors);
-      }
-      const categoriesLoaded = new Set();
-      // if no errors, you can map into the data and create your static pages
-      result.data.allOrangeCountyYaml.edges.forEach(({ node }) => {
-        // create page according to the fetched data
-        createPage({
-          path: `/${fixStringForWindows(node.category)}/${fixStringForWindows(node.title)}`, // your url -> /categories/orgtitle
-          component: path.resolve('./src/templates/resources.js'), // your template component
-          // component: path.resolve('./src/components/pagecontainer.js'),
-          context: {
-            id: node.id,
-            // optional,
-            // data here will be passed as props to the component `this.props.pathContext`,
-            // as well as to the graphql query as graphql arguments.
-          },
-        });
+    const promises = [generateResourcePages(queryResources, queryYamlNames),
+      generateCategoryPages(queryCategories, queryYamlNames),
+    ];
+    // generateGuidePages(queryGuides);
 
-        if (!categoriesLoaded.has(node.category)) {
-          categoriesLoaded.add(node.category);
-          createPage({
-            path: `/${fixStringForWindows(node.category)}/${fixStringForWindows(node.title)}`, // your url -> /categories/orgtitle
-            component: path.resolve('./src/templates/categories.js'), // your template component
-            // component: path.resolve('./src/components/pagecontainer.js'),
-            context: {
-              category: node.category,
-              id: node.id,
-            },
-          });
-        }
-      });
-
-      resolve();
-    });
+    Promise.all(promises).then(() => resolve()).catch((error) => reject(error));
+    resolve();
   });
 };
