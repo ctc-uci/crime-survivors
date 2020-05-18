0f8a390b9588799332239f30ceec6d7959e1b128
Paul Yang <palyang777@gmail.com>
2020-05-18 03:02:32 -0700

    Added filtering unique categories

--- client/src/components/CountyPage/CountyPage.js
+++ client/src/components/CountyPage/CountyPage.js
@@ -1,62 +1,72 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { v4 as uuidv4 } from 'uuid';
 import './CountyPage.css';
 
-const renderCategory = (category) => (
-  <div>{category}</div>
+const countyPagePropTypes = {
+  location: PropTypes.string,
+  categories: PropTypes.arrayOf(
+    PropTypes.shape({ category: PropTypes.string }),
+  ),
+};
+const defaultCountyPageProps = {
+  location: 'LOCATION',
+  categories: [
+    { category: 'hello' },
+  ],
+};
+
+const Location = (location) => (
+  <div className="location">{location}</div>
+);
+
+const Category = (category) => (
+  <div
+    key={uuidv4()}
+    className="category"
+  >
+    {category}
+  </div>
   // Would need a link in order to navigate to categoryPage
   // <a href="https://.../{category}">{category}</a>
   // See Link from gatsby(supposedly faster)
   // See generating path with src/util/commonutil
 );
 
-const Categories = ({ categories }) => (
+const Categories = (categories) => (
   <div>
-    {' '}
-    {categories.map(({ category }) => renderCategory(category))}
-    {' '}
+    {categories.map((category) => Category(category))}
   </div>
 );
-Categories.propTypes = {
-  categories: PropTypes.arrayOf(
-    PropTypes.shape({ category: PropTypes.string }),
-  ),
-};
-Categories.defaultProps = {
-  categories: [
-    { category: 'hello' },
-  ],
+
+const filterDupCategories = (categories) => {
+  const categorySet = new Set();
+  categories.forEach(({ category }) => categorySet.add(category));
+  return [...categorySet];
 };
 
-const CountyPage = ({ location, categories }) => (
-  <div className="page">
-    <div>
-      {/* <Header/> */}
-      Placeholder for Header
-    </div>
-    <div className="lateral">
+const CountyPage = ({ location, categories }) => {
+  const uniqCategories = filterDupCategories(categories);
+  return (
+    <div className="page">
       <div>
-        {/* <Sidebar/>, check version_pool for sidebar component */}
-        Placeholder for Sidebar
+        {/* <Header/> */}
+        Placeholder for Header
       </div>
-      <div>
-        {location}
-        <Categories categories={categories} />
+      <div className="lateral">
+        <div>
+          {/* <Sidebar/>, check version_pool for sidebar component */}
+          Placeholder for Sidebar
+        </div>
+        <div className="content">
+          {Location(location)}
+          {Categories(uniqCategories)}
+        </div>
       </div>
     </div>
-  </div>
-);
-CountyPage.propTypes = {
-  location: PropTypes.string,
-  categories: PropTypes.arrayOf({
-    category: PropTypes.string,
-  }),
+  );
 };
-CountyPage.defaultProps = {
-  location: 'LOCATION',
-  categories: [
-    { category: 'hello' },
-  ],
-};
+CountyPage.propTypes = countyPagePropTypes;
+CountyPage.defaultProps = defaultCountyPageProps;
 
 export default CountyPage;
