a6003b75ff91a2f8e390fbd0ab6c752d2c801221
Paul Yang <palyang777@gmail.com>
2020-05-22 22:53:45 -0700

    Added navbar, escape button, and sidebar elements, also got svgs

--- client/src/components/CountyPage/CountyPage.js
+++ client/src/components/CountyPage/CountyPage.js
@@ -2,14 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './CountyPage.css';
 
+import EscapeButton from '../escape/escapeButton';
+import Navbar from '../header/Navbar';
 import Sidebar from '../sidebar/sidebar';
+import { Categories, categoryTypes, categoryDefault } from './Category/category';
 // import { Quotes, quotesTypes, quotesDefault } from './Quotes/Quotes'
+
+import pinkwave1 from './images/pink-wave-1.svg';
+import pinkwave2 from './images/pink-wave-2.svg';
+import pinkwave3 from './images/pink-wave-3.svg';
+import pinkwave4 from './images/pink-wave-4.svg';
+import pinkwave5 from './images/pink-wave-5.svg';
+import pinkwave6 from './images/pink-wave-6.svg';
 
 const countyPageTypes = {
   location: PropTypes.string,
   resources: PropTypes.arrayOf(
     PropTypes.shape({
-      category: PropTypes.string,
+      category: categoryTypes.category,
       title: PropTypes.string,
       id: PropTypes.string,
     }),
@@ -22,7 +32,7 @@ const countyPageDefault = {
   location: 'LOCATION',
   resources: [
     {
-      category: 'CATEGORY',
+      category: categoryDefault,
       title: 'TITLE',
       id: 'ID',
     },
@@ -30,9 +40,22 @@ const countyPageDefault = {
   // quotes: quotesDefault
 };
 
-const Location = (location) => (
-  <div className="location">{location}</div>
+const Location = ({ location }) => (
+  <div className="location">
+    {location}
+    {' '}
+    Guide
+  </div>
 );
+Location.propTypes = countyPageTypes.location;
+Location.defaultProps = countyPageDefault.location;
+
+
+function filterDups(resources) {
+  const categorySet = new Set();
+  resources.forEach(({ category }) => categorySet.add(category));
+  return [...categorySet];
+}
 
 // Think all these transforms are required to fit PropTypes in sidebar
 function truncateForSidebar(resources) {
@@ -62,13 +85,12 @@ function formSidebarData(resources) {
     if (!(category in categoryGroups)) {
       categoryGroups[category] = [];
     }
-    // TO-DO: THIS NULL CHECK IS TEMPORARY -> This is a problem with data
-    if (title != null) {
-      categoryGroups[category].push({
-        title,
-        id,
-      });
-    }
+    // if (title != null) {
+    categoryGroups[category].push({
+      title,
+      id,
+    });
+    // }
   });
   const sidebarData = [];
   Object.keys(categoryGroups).forEach((category) => {
@@ -77,26 +99,37 @@ function formSidebarData(resources) {
       resources: truncateForSidebar(categoryGroups[category]),
     });
   });
-  return sidebarData;
+  return { group: sidebarData };
 }
 
 const CountyPage = ({ location, resources }) => {
+  const categories = filterDups(resources);
   const sidebarData = formSidebarData(resources);
+
   return (
     <div className="page">
-      <div>
-        {/* <Header/> */}
-        Placeholder for Header
-      </div>
+      <EscapeButton />
       <div className="lateral">
         <div>
           <Sidebar
-            sidebarData={sidebarData}
+            props={{ sidebarData }}
           />
         </div>
         <div className="content">
-          {Location(location)}
+          <Navbar />
+          <Location location={location} />
+          <Categories categories={categories} />
           {/* <Quotes quotes={quotes}/> */}
+          {/*
+            Each duo is laid on top of one another,
+            check figma for possible positioning
+          */}
+          <img src={pinkwave1} alt="" />
+          <img src={pinkwave2} alt="" />
+          <img src={pinkwave3} alt="" />
+          <img src={pinkwave4} alt="" />
+          <img src={pinkwave5} alt="" />
+          <img src={pinkwave6} alt="" />
         </div>
       </div>
     </div>
