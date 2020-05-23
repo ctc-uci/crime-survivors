753ef388f15fb29a5e6c3d21e54b7c256beb06b3
Leon <leonlin.48@gmail.com>
2020-05-22 22:43:31 -0700

    left side of footer looks OK

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,42 +1,45 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
-import TwitterIcon from '../../images/twitter-icon.png';
-import FacebookIcon from '../../images/facebook-icon.png';
+import TwitterIcon from '../../images/twitter-icon.svg';
+import FacebookIcon from '../../images/facebook-icon.svg';
 import GooglePlusIcon from '../../images/googleplus-icon.png';
-import YoutubeIcon from '../../images/youtube-icon.png';
-import InstagramIcon from '../../images/instagram-icon.png';
+import YoutubeIcon from '../../images/youtube-icon.svg';
+import InstagramIcon from '../../images/instagram-icon.svg';
 import './Footer.css';
 
 
 const Footer = () => (
   <div className="footer">
-    <div className="footer-info">
-      @ 2003-2019 Crime Survivors, Inc.
-      {' '}
-      All Rights Reserved.
-      <br />
-      Website by Commit the Change
-    </div>
-    <div className="footer-social-media">
-      <a className="fb-link" href="https://www.facebook.com/crimesurvivors">
-        <img src={FacebookIcon} id="fb-icon" alt="Facebook" />
-      </a>
-      {' '}
-      <a className="twitter-link" href="https://twitter.com/crimesurvivors">
-        <img src={TwitterIcon} id="twitter-icon" alt="Twitter" />
-      </a>
-      {' '}
-      <a className="gplus-link" href="https://plus.google.com/+CrimesurvivorsOrg/">
-        <img src={GooglePlusIcon} id="gplus-icon" alt="Google Plus" />
-      </a>
-      {' '}
-      <a className="yt-link" href="https://www.youtube.com/crimesurvivor">
-        <img src={YoutubeIcon} id="yt-icon" alt="Youtube" />
-      </a>
-      {' '}
-      <a className="ig-link" href="https://www.instagram.com/crimesurvivors/">
-        <img src={InstagramIcon} id="ig-icon" alt="Instagram" />
-      </a>
+    <div className="footer-navigation">
+      <div className="footer-info">
+        <b id="subtitle">Crime Survivors Resources</b>
+        <br />
+        <Link to="/index/">Home</Link>
+        <br />
+        <Link to="/index/">Find your County</Link>
+        <br />
+        <Link to="/index/">General Guides</Link>
+        <br />
+        <Link to="/index/">Contact Us</Link>
+      </div>
+      <div className="footer-social-media">
+        <a className="fb-link" href="https://www.facebook.com/crimesurvivors">
+          <img src={FacebookIcon} id="fb-icon" alt="Facebook" />
+        </a>
+        <a className="ig-link" href="https://www.instagram.com/crimesurvivors/">
+          <img src={InstagramIcon} id="ig-icon" alt="Instagram" />
+        </a>
+        <a className="yt-link" href="https://www.youtube.com/crimesurvivor">
+          <img src={YoutubeIcon} id="yt-icon" alt="Youtube" />
+        </a>
+        <a className="twitter-link" href="https://twitter.com/crimesurvivors">
+          <img src={TwitterIcon} id="twitter-icon" alt="Twitter" />
+        </a>
+        <a className="gplus-link" href="https://plus.google.com/+CrimesurvivorsOrg/">
+          <img src={GooglePlusIcon} id="gplus-icon" alt="Google Plus" />
+        </a>
+      </div>
     </div>
   </div>
 );
