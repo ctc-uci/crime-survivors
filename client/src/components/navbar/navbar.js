f7713b5c641ec551bf21d21b978647a83f7a89e2
justinekoa <justinekoa@gmail.com>
2020-05-28 16:38:47 -0700

    fixed event listener

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -1,66 +1,32 @@
 /* eslint-disable react/jsx-indent */
 /* eslint-disable no-undef */
-// import React, { useEffect, useState } from 'react';
-import React from 'react';
+import React, { useEffect } from 'react';
+// import React from 'react';
 // import { Link } from 'gatsby';
 
 import './navbar.css';
 import logo from '../../images/cs-logo.png';
 
-
-// // Hook
-// function useWindowSize() {
-//   // const isClient = typeof window === 'object';
-
-//   function getSize() {
-//     return {
-//       width: window.innerHeight,
-//       height: window.innerHeight,
-//     };
-//   }
-
-//   const [windowSize, setWindowSize] = useState(getSize);
-
-//   useEffect(() => {
-//     // if (!isClient) {
-//     //   return false;
-//     // }
-
-//     function handleResize() {
-//       setWindowSize(getSize());
-//     }
-
-//     window.addEventListener('resize', handleResize);
-//     return () => window.removeEventListener('resize', handleResize);
-//   }, []); // Empty array ensures that effect is only run on mount and unmount
-
-//   return windowSize;
-// }
-
-// function getWidth() {
-//   const windowWidth = useWindowSize().width;
-//   const sidebarWidth = document.querySelector('.sidebar').offsetWidth; +20;
-//   const navWidth = windowWidth - sidebarWidth;
-//   return navWidth;
-// }
-
 function resizeMainContainer() {
-  // const currentMainWidth = document.querySelector('.main-container').offsetWidth;
   const screenWidth = document.querySelector('.escape-container').offsetWidth;
   const sidebarWidth = document.querySelector('.sidebar-container').offsetWidth;
 
   const mainContainerWidth = screenWidth - sidebarWidth;
   const w = `${mainContainerWidth.toString()}px`;
   document.querySelector('.main-container').style.width = w;
-  // const mainContainerWidth = screenWidth - sidebarWidth;
-  // const w = `${mainContainerWidth.toString()}px`;
-  // document.querySelector('.main-container').style.width = w;
 }
 
-window.addEventListener('resize', resizeMainContainer);
-
-const Navbar = () => (
-  <div className="nav-bar" id="nav-bar" onLoad="resizeMainContainer()">
+const Navbar = () => {
+  useEffect(() => {
+    window.addEventListener('resize', resizeMainContainer);
+    // Resized
+    return () => {
+      // Clean Up
+      window.removeEventListener('resize', resizeMainContainer);
+    };
+  }, []);
+  return (
+  <div className="nav-bar" id="nav-bar">
   {/* <div className="nav-bar" id="nav-bar"> */}
     {/* links are nothing for now */}
     <div className="logo">
@@ -83,6 +49,7 @@ const Navbar = () => (
       </li>
     </ul>
   </div>
-);
+  );
+};
 
 export default Navbar;
