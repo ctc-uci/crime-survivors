613b42bcb3c112e5960c9f525d0add0122d434ab
Leon <leonlin.48@gmail.com>
2020-05-28 19:24:46 -0700

    added larger margin-bottom to disclaimer text

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -10,7 +10,7 @@ import './Footer.css';
 
 
 const Footer = () => {
-  const { title, body } = useStaticQuery(graphql`
+  const { title, body: { body } } = useStaticQuery(graphql`
     query DislaimerQuery {
       allContentfulDisclaimer {
         edges {
@@ -26,8 +26,8 @@ const Footer = () => {
 
   return (
     <div className="footer">
-      <div className="footer-navigation">
-        <div className="footer-local">
+      <div id="footer-navigation">
+        <div id="footer-local">
           <b id="subtitle">Crime Survivors Resources</b>
           <br />
           <Link to="/index/">Home</Link>
@@ -38,28 +38,28 @@ const Footer = () => {
           <br />
           <Link to="/index/">Contact Us</Link>
         </div>
-        <div className="footer-social-media">
-          <a className="fb-link" href="https://www.facebook.com/crimesurvivors">
+        <div id="footer-social-media">
+          <a id="fb-link" href="https://www.facebook.com/crimesurvivors">
             <img src={FacebookIcon} id="fb-icon" alt="Facebook" />
           </a>
-          <a className="ig-link" href="https://www.instagram.com/crimesurvivors/">
+          <a id="ig-link" href="https://www.instagram.com/crimesurvivors/">
             <img src={InstagramIcon} id="ig-icon" alt="Instagram" />
           </a>
-          <a className="yt-link" href="https://www.youtube.com/crimesurvivor">
+          <a id="yt-link" href="https://www.youtube.com/crimesurvivor">
             <img src={YoutubeIcon} id="yt-icon" alt="Youtube" />
           </a>
-          <a className="twitter-link" href="https://twitter.com/crimesurvivors">
+          <a id="twitter-link" href="https://twitter.com/crimesurvivors">
             <img src={TwitterIcon} id="twitter-icon" alt="Twitter" />
           </a>
-          {/* <a className="gplus-link" href="https://plus.google.com/+CrimesurvivorsOrg/">
+          {/* <a id="gplus-link" href="https://plus.google.com/+CrimesurvivorsOrg/">
             <img src={GooglePlusIcon} id="gplus-icon" alt="Google Plus" />
           </a> */}
         </div>
       </div>
-      <div className="disclaimer">
+      <div id="disclaimer">
         <b>{title}</b>
         <div id="disclaimer-text">
-          {body.body}
+          {body}
         </div>
       </div>
     </div>
