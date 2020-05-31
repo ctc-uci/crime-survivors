dc4334852e8145a99cb8add68ff837a94aa07f1e
Albert Zhang <albert@Alberts-MacBook-Air.local>
2020-05-31 07:15:21 -0700

    allow sidebar to open/cloose on both mobile and desktop with resizing

--- client/src/components/sidebar/sidebar.js
+++ client/src/components/sidebar/sidebar.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+/* eslint-disable no-undef */
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
 import Category from './category/category';
@@ -6,29 +7,67 @@ import './sidebar.css';
 
 const Sidebar = ({ props }) => {
   // TODO: Use resourceId/CategoryId to jump to section and/or hide/collapse other categories
-  // eslint-disable-next-line react/prop-types
 
   const [sidebarOpen, setSidebarState] = useState(true);
   const { sidebarData } = props;
 
-  // if(fullScreenSidebar) {
-  //   style = {
-  //     sidebar : {
+  let main;
+  let sidebarContainer;
+  let sidebar;
+  let mobile;
 
-  //     },
-  //     'main-container' : {
-  //       display: none
-  //     }
+  function check() {
+    if (sidebarOpen) {
+      // console.log(mobile);
+      // console.log(document.querySelector('.escape-container').offsetWidth);
+      if (mobile) {
+        sidebar.style.display = 'block';
+        sidebar.style.width = '100%';
+        sidebarContainer.style.width = '100%';
+        main.style.display = 'none';
+      } else {
+        // main.style.display = 'block';
+        sidebar.style.display = 'block';
+        sidebar.style.width = '272';
+        sidebarContainer.style.width = 'initial';
+        main.style.display = 'block';
+      }
+    } else {
+      sidebar.style.display = 'none';
+      sidebarContainer.style.width = '0%';
+      main.style.display = 'block';
+      main.style.width = '100%';
+    }
+  }
 
-  //   }
-  // }
+
+  function resizeWindow() {
+    main = document.querySelector('.main-container');
+    sidebarContainer = document.querySelector('.sidebar-container');
+    sidebar = document.querySelector('.sidebar');
+    mobile = document.querySelector('.escape-container').offsetWidth <= 600;
+
+    check();
+  }
+
+
+  useEffect(() => {
+    main = document.querySelector('.main-container');
+    sidebarContainer = document.querySelector('.sidebar-container');
+    sidebar = document.querySelector('.sidebar');
+    mobile = document.querySelector('.escape-container').offsetWidth <= 600;
+    window.addEventListener('resize', resizeWindow);
+
+    check();
+  });
+
 
   return (
     <div className="sidebar-container">
-      <button type="button" className="sidebar-button" onClick={() => { setSidebarState(!sidebarOpen); }}>OPEN SIDEBAR</button>
-      <div className={`sidebar${sidebarOpen ? '' : ' hidden'}`}>
+      <button type="button" className="sidebar-button" onClick={() => { setSidebarState(true); }}>OPEN SIDEBAR</button>
+      <div className="sidebar">
         <h1>Title</h1>
-        <button type="button" onClick={() => { setSidebarState(!sidebarOpen); }}>hide</button>
+        <button type="button" onClick={() => { setSidebarState(false); }}>hide</button>
         {sidebarData.group.map((category) => (
           <div key={uuidv4()}>
             {/* <p>
