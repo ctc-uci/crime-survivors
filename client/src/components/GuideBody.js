6c3c42973fff6abf6275f6fac3d54ea8fccf9d49
Harrison Huang <harrisonhuang00@gmail.com>
2020-07-18 03:32:36 +0000

    Hh guide styling update

--- client/src/components/GuideBody.js
+++ client/src/components/GuideBody.js
@@ -3,12 +3,23 @@ import { v4 as uuidv4 } from 'uuid';
 import PropTypes from 'prop-types';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { BLOCKS } from '@contentful/rich-text-types';
-import '../styles/CategoryPage.css';
+import '../styles/guide-body.css';
 
 function GuideBody({ contentfulGuide }) {
   // console.log(contentfulGuide);
   const { title, generalDescription, guideSections } = contentfulGuide;
   const generalDescriptionText = generalDescription ? generalDescription.generalDescription : '';
+
+  let guideSectionsLeft = null;
+  let guideSectionsRight = null;
+
+  if (guideSections) {
+    /* eslint-disable react/prop-types */
+    const half = Math.ceil(guideSections.length / 2);
+    guideSectionsLeft = guideSections.slice(0, half);
+    guideSectionsRight = guideSections.slice(half + 1);
+    /* eslint-disable react/prop-types */
+  }
 
   const options = {
     renderNode: {
@@ -18,18 +29,33 @@ function GuideBody({ contentfulGuide }) {
   };
 
   return (
-    <div id="guide-page">
-      <h1>{title}</h1>
-      <p>{generalDescriptionText}</p>
+    <div className="guide-page">
+      <p className="guide-title">General Guide</p>
+      <p className="guide-subtitle">{title}</p>
+      <p className="guide-summary">{generalDescriptionText}</p>
       {/* TODO: figure out why eslint thinks guideSections.map need proptypes */}
-      {guideSections && guideSections.map((guideSection) => { // eslint-disable-line
-        const { content: { content } } = guideSection;
-        return (
-          <div className="guide-section" key={uuidv4()}>
-            {documentToReactComponents(JSON.parse(content), options)}
-          </div>
-        );
-      })}
+      <div className="guide-container">
+        <div className="guide-column">
+          {guideSectionsLeft && guideSectionsLeft.map((guideSection) => { // eslint-disable-line
+            const { content: { content } } = guideSection;
+            return (
+              <div className="guide-section" key={uuidv4()}>
+                {documentToReactComponents(JSON.parse(content), options)}
+              </div>
+            );
+          })}
+        </div>
+        <div className="guide-column">
+          {guideSectionsRight && guideSectionsRight.map((guideSection) => { // eslint-disable-line
+            const { content: { content } } = guideSection;
+            return (
+              <div className="guide-section" key={uuidv4()}>
+                {documentToReactComponents(JSON.parse(content), options)}
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
