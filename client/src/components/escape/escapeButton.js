0f99d3c247875585ec2732b9f02ae9380e6919ed
justinekoa <justinekoa@gmail.com>
2020-07-18 18:05:50 -0700

    update styling

--- client/src/components/escape/escapeButton.js
+++ client/src/components/escape/escapeButton.js
@@ -4,7 +4,7 @@ import './escapeButton.css';
 const EscapeButton = () => (
   <div className="escape-container" id="escape-container">
     <a href="https://www.allrecipes.com/recipe/10813/best-chocolate-chip-cookies/">
-      Click HERE if you need to leave this site quickly
+      CLICK HERE if you need to leave this site quickly
     </a>
   </div>
 );
