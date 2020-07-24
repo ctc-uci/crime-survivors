94d5c3238f6f8cca4eea9daefa615a7016942b0d
Paul Yang <palyang777@gmail.com>
2020-07-23 21:31:57 -0700

    edited

--- client/src/content/convjson.py
+++ client/src/content/convjson.py
@@ -1,10 +1,11 @@
 import yaml, json, pathlib
 
 def dump_to_yaml(filename):
-    if filename.split('.')[1] != "yaml":
+    parts = filename.split('.')
+    if len(parts) != 2 or parts[1] != "yaml":
         return
 
-    name = filename.split('.')[0]
+    name = parts[0]
     with open(filename, 'r', encoding='utf-8') as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
         
@@ -13,5 +14,5 @@ def dump_to_yaml(filename):
             json.dump(data[:len(data)], f)
             print(filename, len(data))
 
-for file in pathlib.Path(".").iterdir():
+for file in pathlib.Path('.').iterdir():
     dump_to_yaml(file.name)
