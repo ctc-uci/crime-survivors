1c5f4f8315a06c9d99b91c52c2801d9aca8dfdd1
Harrison Huang <harrisonhuang00@gmail.com>
2020-09-30 00:26:23 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- src/templates/GuidePage.tsx
+++ src/templates/GuidePage.tsx
@@ -17,7 +17,7 @@ const GuidePage: React.FC<GuidePageProps> = ({ pageContext, data }) => {
       header={<Navbar location={{ pathname: '/guide' }} />}
       content={<GuideContent generalGuide={guideData} category={category} />}
       enableLeftSidebar
-      leftSidebar={<LeftSidebar sidebarData={sidebarData} location="guides" title="General Guides" /* TODO: export to const */ />}
+      leftSidebar={<LeftSidebar sidebarData={sidebarData} location="guide" title="General Guides" /* TODO: export to const */ />}
       enableRightSidebar
       rightSidebar={
         <RightSidebar category={category} resources={rightSidebarData} />
