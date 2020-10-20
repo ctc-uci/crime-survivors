33a74a038b1601c88891525cf1dbbc184b9fe4a2
Albert Zhang <aalbertzhang@gmail.com>
2020-10-20 04:21:03 +0000

    mobile-leftbar

--- src/common/utils/constants.d.ts
+++ src/common/utils/constants.d.ts
@@ -3,3 +3,4 @@ export const GUIDES_PATH_PREFIX: string;
 export const FIND_COUNTY_SECTION_ID: string;
 export const CONTACT_PATH_PREFIX: string;
 export const DONATE_PATH: string;
+export const MOBILE_WIDTH: number;
--- src/templates/CountyPage.tsx
+++ src/templates/CountyPage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react';
 import { graphql } from 'gatsby';
 import { CountyPageProps, CountyPageDefaultProps } from './CountyPage.interface';
